151a29b92e9404b8ffd457244049feed9480ee77
Ilshae <pruszynskaingrid@outlook.com>
2023-12-14 20:01:27 +0100

    Yacht tests 2

--- cypress/e2e/home.js
+++
@@ -1,9 +0,0 @@
-describe("Homepage", () => {
-  beforeEach(() => {
-    cy.visit("http://localhost:5173/")
-  })
-
-  it("displays footer", () => {
-    cy.contains("Przystań Zielona Zatoka 11-500 Bogaczewo")
-  })
-})
